13d9de748569e16253cf92b2286d2d6122dbbd76
PotcFdk <PotcFdk@gmx.net>
2013-09-04 20:27:08 +0200

    Redesigned list generation process and the format it saves the list in.

--- main.cpp
+++ main.cpp
@@ -16,21 +16,25 @@ using namespace std;
 
 // Defines
 
-typedef tuple<string, string, double> SoundInfo;
+typedef tuple<string, double> SoundInfo;
 typedef deque<SoundInfo> SoundList;
+typedef pair<string, SoundList> NamedSoundList;
+typedef deque<NamedSoundList> SoundMasterList;
+
+
+#define LISTPATH "lua/chatsounds/lists_nosend"
+#define SOUNDPATH "sound/chatsounds/autoadd"
 
 
 
-
-
-double GetSoundDuration(boost::filesystem::path path)
+double GetSoundDuration(boost::filesystem::path path) // Gets the duration of a sound.
 {
     HSTREAM sound = BASS_StreamCreateFile(false, path.c_str(), 0, 0, BASS_STREAM_PRESCAN);
     QWORD bytecount = BASS_ChannelGetLength(sound, BASS_POS_BYTE);
     return BASS_ChannelBytes2Seconds(sound, bytecount);
 }
 
-SoundInfo GetSoundInfo(boost::filesystem::path path, bool soundgroup)
+SoundInfo GetSoundInfo(boost::filesystem::path path) // Assembles an infolist about a sound.
 {
     if (path.has_extension())
     {
@@ -39,72 +43,80 @@ SoundInfo GetSoundInfo(boost::filesystem::path path, bool soundgroup)
 
         if (ext == ".ogg" || ext == ".mp3" || ext == ".wav")
         {
-            string name;
-            if (soundgroup)
-                name = path.parent_path().filename().string();
-            else
-                name = path.filename().replace_extension("").string();
-
-            double duration = GetSoundDuration(path);
-
-            return SoundInfo(path.generic_string(), name, duration);
+            return SoundInfo(path.generic_string(),GetSoundDuration(path));
         }
     }
-    return SoundInfo("","",0);
+    return SoundInfo("",0);
 }
 
-void ProcessSounds(boost::filesystem::path path, bool soundgroup, SoundList * list)
-{
-    for(boost::filesystem::directory_iterator it(path); it != boost::filesystem::directory_iterator(); ++it)
-    {
-        if ( is_directory(it->status()) )
-        {
-            ProcessSounds(it->path(), true, list);
-        }
-        else
-        {
-            SoundInfo soundinfo = GetSoundInfo(it->path(), soundgroup);
-            if (get<2>(soundinfo) > 0)
-                list->push_back(soundinfo);
-        }
-    }
-}
-
-SoundList ProcessSoundFolders(boost::filesystem::path path)
+NamedSoundList ProcessSoundGroup(boost::filesystem::path path)
 {
     SoundList list;
     for(boost::filesystem::directory_iterator it(path); it != boost::filesystem::directory_iterator(); ++it)
     {
-        if ( is_directory(it->status()) )
+        if (boost::filesystem::is_regular_file(it->status()))
         {
-            ProcessSounds(it->path(), false, &list);
+            list.push_back(GetSoundInfo(it->path()));
         }
     }
+    NamedSoundList nlist(path.filename().string(), list);
+    return nlist;
+}
 
-    for(SoundList::const_iterator it=list.begin() ; it < list.end(); it++ )
+void ProcessSounds(boost::filesystem::path path, SoundMasterList * list) // Scans a subdirectory and compiles all the soundinfos into a list.
+{
+    for(boost::filesystem::directory_iterator it(path); it != boost::filesystem::directory_iterator(); ++it)
     {
-        SoundInfo sndinfo = *it;
-        cout << "processed file: " << get<1>(sndinfo) << endl;
+        if ( is_directory(it->status()) )
+        {
+            list->push_back(ProcessSoundGroup(it->path()));
+        }
+        else if ( boost::filesystem::is_regular_file(it->status()) )
+        {
+            SoundInfo soundinfo = GetSoundInfo(it->path());
+            if (get<1>(soundinfo) > 0)
+            {
+                SoundList sl;
+                sl.push_back(soundinfo);
+                list->push_back(NamedSoundList(it->path().filename().replace_extension("").string(),sl));
+            }
+        }
+    }
+}
+
+SoundMasterList ProcessSoundFolder(boost::filesystem::path path)
+{
+    SoundMasterList list;
+
+    if (is_directory(path))
+    {
+        ProcessSounds(path, &list);
     }
 
     return list;
 }
 
-void BuildSoundList(boost::filesystem::path path)
+void BuildSoundList(SoundMasterList list, string listname)
 {
-    SoundList list = ProcessSoundFolders(path);
+    string soundlist = "c.StartList(\"" + listname + "\")\n";
 
-    string soundlist = "{";
-
-    for(SoundList::const_iterator it=list.begin() ; it < list.end(); it++ )
+    for(SoundMasterList::const_iterator it=list.begin() ; it < list.end(); it++ )
     {
-        SoundInfo sndinfo = *it;
-        soundlist += "{\"" + get<0>(sndinfo) + "\",\"" + get<1>(sndinfo) + "\"," + boost::lexical_cast<std::string>(get<2>(sndinfo)) + "},";
+        SoundList sndlist = get<1>(*it);
+        soundlist += "L[\"" + get<0>(*it) + "\"]={";
+        for(SoundList::const_iterator it2=sndlist.begin() ; it2 < sndlist.end(); it2++ )
+        {
+            SoundInfo sndinfo = *it2;
+            soundlist += "{path=\"" + get<0>(sndinfo) + "\",length=" + boost::lexical_cast<std::string>(get<1>(sndinfo)) + "},";
+        }
+        soundlist += "}\n";
+        //SoundInfo sndinfo = *it;
+        //soundlist += "{\"" + get<0>(sndinfo) + "\",\"" + get<1>(sndinfo) + "\"," + boost::lexical_cast<std::string>(get<2>(sndinfo)) + "},";
     }
 
-    soundlist += "}";
+    soundlist += "c.EndList()";
 
-    std::ofstream f( "soundlist.txt" );
+    std::ofstream f( string(LISTPATH) + "/" + listname + ".txt" );
     if ( !f.fail() )
     {
         f << soundlist;
@@ -112,11 +124,25 @@ void BuildSoundList(boost::filesystem::path path)
     }
 }
 
+void ProcessSoundFolders(boost::filesystem::path path)
+{
+    for(boost::filesystem::directory_iterator it(path); it != boost::filesystem::directory_iterator(); ++it)
+    {
+        if ( is_directory(it->status()) )
+        {
+            SoundMasterList list = ProcessSoundFolder(it->path());
+            BuildSoundList(list, it->path().filename().string());
+            cout << "Generated List: " << it->path().filename() << endl;
+        }
+    }
+}
+
+
 
 int main()
 {
     BASS_Init(-1,44100,BASS_DEVICE_FREQ,0,NULL);
 
-    BuildSoundList(boost::filesystem::path("sound/chatsounds/autoadd"));
+    ProcessSoundFolders(boost::filesystem::path(SOUNDPATH));
     return 0;
 }
