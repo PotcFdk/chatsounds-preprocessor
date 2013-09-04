69a630a728efc72bba3bf7b0f0f08a297f3f8cd0
PotcFdk <PotcFdk@gmx.net>
2013-09-04 17:13:53 +0200

    Correct levels of iteration.

--- main.cpp
+++ main.cpp
@@ -6,18 +6,33 @@
 
 using namespace std;
 
-void GetSoundInfo(boost::filesystem::path path)
+void GetSoundInfo(boost::filesystem::path path, bool soundgroup)
 {
-
+    cout << path << endl;
 }
 
-void ProcessSounds(boost::filesystem::path path)
+void ProcessSounds(boost::filesystem::path path, bool soundgroup)
 {
     for(boost::filesystem::directory_iterator it(path); it != boost::filesystem::directory_iterator(); ++it)
     {
         if ( is_directory(it->status()) )
         {
-            boost::filesystem::path pp = it->path();
+            ProcessSounds(it->path(), true);
+        }
+        else
+        {
+            GetSoundInfo(it->path(), soundgroup);
+        }
+    }
+}
+
+void ProcessSoundFolders(boost::filesystem::path path)
+{
+    for(boost::filesystem::directory_iterator it(path); it != boost::filesystem::directory_iterator(); ++it)
+    {
+        if ( is_directory(it->status()) )
+        {
+            ProcessSounds(it->path(), false);
         }
     }
 }
@@ -25,6 +40,6 @@ void ProcessSounds(boost::filesystem::path path)
 
 int main()
 {
-    ProcessSounds(boost::filesystem::path("sound/chatsounds/autoadd"));
+    ProcessSoundFolders(boost::filesystem::path("sound/chatsounds/autoadd"));
     return 0;
 }
