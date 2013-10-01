e492b6776a380b8ddef0da457748283fc682d78b
PotcFdk <PotcFdk@gmx.net>
2013-10-01 16:02:22 +0200

    Sound names are now saved in lowercase.

--- version.h
+++ version.h
@@ -4,10 +4,10 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "23";
-	static const char MONTH[] = "09";
+	static const char DATE[] = "01";
+	static const char MONTH[] = "10";
 	static const char YEAR[] = "2013";
-	static const char UBUNTU_VERSION_STYLE[] = "13.09";
+	static const char UBUNTU_VERSION_STYLE[] = "13.10";
 	
 	//Software Status
 	static const char STATUS[] = "Beta";
@@ -16,14 +16,14 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR = 0;
 	static const long MINOR = 3;
-	static const long BUILD = 2;
+	static const long BUILD = 3;
 	static const long REVISION = 0;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT = 360;
-	#define RC_FILEVERSION 0,3,2,0
-	#define RC_FILEVERSION_STRING "0, 3, 2, 0\0"
-	static const char FULLVERSION_STRING[] = "0.3.2.0";
+	static const long BUILDS_COUNT = 362;
+	#define RC_FILEVERSION 0,3,3,0
+	#define RC_FILEVERSION_STRING "0, 3, 3, 0\0"
+	static const char FULLVERSION_STRING[] = "0.3.3.0";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY = 1;
