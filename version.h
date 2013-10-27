51e23ed3953ae5c1b173cc1d07f8c6a0ad09a6a4
PotcFdk <PotcFdk@gmx.net>
2013-10-27 02:45:20 +0100

    Paths are now sorted before generating list.

--- version.h
+++ version.h
@@ -4,7 +4,7 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "01";
+	static const char DATE[] = "27";
 	static const char MONTH[] = "10";
 	static const char YEAR[] = "2013";
 	static const char UBUNTU_VERSION_STYLE[] = "13.10";
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR = 0;
 	static const long MINOR = 3;
-	static const long BUILD = 3;
-	static const long REVISION = 0;
+	static const long BUILD = 4;
+	static const long REVISION = 10;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT = 362;
-	#define RC_FILEVERSION 0,3,3,0
-	#define RC_FILEVERSION_STRING "0, 3, 3, 0\0"
-	static const char FULLVERSION_STRING[] = "0.3.3.0";
+	static const long BUILDS_COUNT = 374;
+	#define RC_FILEVERSION 0,3,4,10
+	#define RC_FILEVERSION_STRING "0, 3, 4, 10\0"
+	static const char FULLVERSION_STRING[] = "0.3.4.10";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY = 1;
+	static const long BUILD_HISTORY = 2;
 	
 
 }
