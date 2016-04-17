fbe3acc333d53d1f8b5643e825b27188a4a58b95
PotcFdk <PotcFdk@gmx.net>
2016-04-17 23:52:58 +0200

    This is version 0.6.5.

--- version.h
+++ version.h
@@ -4,10 +4,10 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "14";
-	static const char MONTH[] = "02";
+	static const char DATE[] = "17";
+	static const char MONTH[] = "04";
 	static const char YEAR[] = "2016";
-	static const char UBUNTU_VERSION_STYLE[] =  "16.02";
+	static const char UBUNTU_VERSION_STYLE[] =  "16.04";
 	
 	//Software Status
 	static const char STATUS[] =  "Beta";
@@ -16,14 +16,14 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR  = 0;
 	static const long MINOR  = 6;
-	static const long BUILD  = 4;
+	static const long BUILD  = 5;
 	static const long REVISION  = 0;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 778;
-	#define RC_FILEVERSION 0,6,4,0
-	#define RC_FILEVERSION_STRING "0, 6, 4, 0\0"
-	static const char FULLVERSION_STRING [] = "0.6.4.0";
+	static const long BUILDS_COUNT  = 906;
+	#define RC_FILEVERSION 0,6,5,0
+	#define RC_FILEVERSION_STRING "0, 6, 5, 0\0"
+	static const char FULLVERSION_STRING [] = "0.6.5.0";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY  = 2;
