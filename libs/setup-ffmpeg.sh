b084d2cbd81469d6c74bb4da3c3c75df48c740dd
PotcFdk <PotcFdk@gmx.net>
2017-07-09 14:49:09 +0200

    libavformat: enable file protocol

--- libs/setup-ffmpeg.sh
+++ libs/setup-ffmpeg.sh
@@ -7,7 +7,7 @@ cd ffmpeg-3.3.1
 echo "Patching ffmpeg for our uses..."
 patch < ../patches/ffmpeg-configure-no-autodetect-externals.patch
 echo "Configuring ffmpeg (this might take a while)..."
-./configure --disable-all --enable-libmp3lame --enable-avformat --enable-avcodec --enable-demuxer={mp3,ogg,wav} --enable-decoder={vorbis,mp3,pcm_s16le} --enable-static --disable-shared --disable-debug --enable-stripping --prefix="../ffmpeg"
+./configure --disable-all --enable-protocol=file --enable-libmp3lame --enable-avformat --enable-avcodec --enable-demuxer={mp3,ogg,wav} --enable-decoder={vorbis,mp3,pcm_s16le} --enable-static --disable-shared --disable-debug --enable-stripping --prefix="../ffmpeg"
 echo "Building ffmpeg..."
 mkdir ../ffmpeg
 make -j && make install
