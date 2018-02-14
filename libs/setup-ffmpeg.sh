3a04368b7fe9709480ac2a28e9a04c06ffe8b18a
PotcFdk <PotcFdk@gmx.net>
2018-02-14 21:45:20 +0100

    Update local ffmpeg version used in setup-ffmpeg.sh ffmpeg-3.3.1 -> ffmpeg-3.4.2

--- libs/setup-ffmpeg.sh
+++ libs/setup-ffmpeg.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 echo "Downloading ffmpeg..."
-wget --no-clobber https://ffmpeg.org/releases/ffmpeg-3.3.1.tar.xz
+wget --no-clobber https://ffmpeg.org/releases/ffmpeg-3.4.2.tar.xz
 echo "Extracting ffmpeg..."
-tar xvf ffmpeg-3.3.1.tar.xz
-cd ffmpeg-3.3.1
-echo "Patching ffmpeg for our uses..."
-patch < ../patches/ffmpeg-configure-no-autodetect-externals.patch
+tar xvf ffmpeg-3.4.2.tar.xz
+cd ffmpeg-3.4.2
 echo "Configuring ffmpeg (this might take a while)..."
-./configure --disable-all --enable-protocol=file --enable-libmp3lame --enable-avformat --enable-avcodec --enable-demuxer={mp3,ogg,wav} --enable-decoder={vorbis,mp3,pcm_s16le} --enable-static --disable-shared --disable-debug --enable-stripping --prefix="../ffmpeg"
+./configure --disable-all --disable-network --disable-autodetect --enable-protocol=file --enable-libmp3lame --enable-avformat --enable-avcodec --enable-demuxer={mp3,ogg,wav} --enable-decoder={vorbis,mp3,pcm_s16le} --enable-static --disable-shared --disable-debug --enable-stripping --prefix="../ffmpeg"
 echo "Building ffmpeg..."
 mkdir ../ffmpeg
 make -j && make install
