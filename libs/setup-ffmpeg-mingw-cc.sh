73900de31a0b79aca8f76956b650781fdb650dd8
PotcFdk <PotcFdk@gmx.net>
2019-05-11 19:07:52 +0200

    Remove bcrypt dependency from Windows builds of libavutil.

--- libs/setup-ffmpeg-mingw-cc.sh
+++ libs/setup-ffmpeg-mingw-cc.sh
@@ -5,6 +5,7 @@ echo "Extracting ffmpeg..."
 tar xvf ffmpeg-4.1.3.tar.xz
 cd ffmpeg-4.1.3
 echo "Configuring ffmpeg (this might take a while)..."
+sed -i 's/BCRYPT_RNG_ALGORITHM/BCRYPT_NO_RNG_ALGORITHM/g' configure
 ./configure --arch=x86 --target-os=mingw32 --cross-prefix=i686-w64-mingw32- --disable-all --disable-network --disable-autodetect --enable-protocol=file --enable-libmp3lame --enable-avformat --enable-avcodec --enable-demuxer={mp3,ogg,wav} --enable-decoder={vorbis,mp3,pcm_s16le} --enable-static --disable-shared --disable-debug --enable-stripping --prefix="../ffmpeg"
 echo "Building ffmpeg..."
 mkdir ../ffmpeg
--- libs/setup-ffmpeg-native.sh
+++ libs/setup-ffmpeg-native.sh
@@ -5,6 +5,7 @@ echo "Extracting ffmpeg..."
 tar xvf ffmpeg-4.1.3.tar.xz
 cd ffmpeg-4.1.3
 echo "Configuring ffmpeg (this might take a while)..."
+sed -i 's/BCRYPT_RNG_ALGORITHM/BCRYPT_NO_RNG_ALGORITHM/g' configure
 ./configure --disable-all --disable-network --disable-autodetect --enable-protocol=file --enable-libmp3lame --enable-avformat --enable-avcodec --enable-demuxer={mp3,ogg,wav} --enable-decoder={vorbis,mp3,pcm_s16le} --enable-static --disable-shared --disable-debug --enable-stripping --prefix="../ffmpeg"
 echo "Building ffmpeg..."
 mkdir ../ffmpeg
