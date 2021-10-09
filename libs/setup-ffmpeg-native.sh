566de1e7f9c12d313e20dc335454e1860fe8dbc2
PotcFdk <PotcFdk@gmx.net>
2021-10-10 00:14:05 +0200

    Switch to ffmpeg version 4.4.

--- libs/setup-ffmpeg-native.sh
+++ libs/setup-ffmpeg-native.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "Downloading ffmpeg..."
-wget --no-clobber https://ffmpeg.org/releases/ffmpeg-4.2.2.tar.xz
+wget --no-clobber https://ffmpeg.org/releases/ffmpeg-4.4.tar.xz
 echo "Extracting ffmpeg..."
-tar xvf ffmpeg-4.2.2.tar.xz
-cd ffmpeg-4.2.2
+tar xvf ffmpeg-4.4.tar.xz
+cd ffmpeg-4.4
 echo "Configuring ffmpeg (this might take a while)..."
 sed -i 's/BCRYPT_RNG_ALGORITHM/BCRYPT_NO_RNG_ALGORITHM/g' configure
 ./configure --disable-all --disable-network --disable-autodetect --enable-protocol=file --enable-libmp3lame --enable-avformat --enable-avcodec --enable-demuxer={mp3,ogg,wav} --enable-decoder={vorbis,mp3,pcm_s16le} --enable-static --disable-shared --disable-debug --enable-stripping --prefix="../ffmpeg"
